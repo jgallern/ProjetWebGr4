a2002a389ddf59f3b8108e41070434809aacd30f
JG <jg@jg.home>
2024-03-04 15:38:23 +0100

    debuts php

---
+++ connect.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<h1>nous traitons vos données...</h1>
+<?php
+$id = $_POST['id_connection'];
+$password = $_POST['password_connection'];
+echo("ahahaahahah je leak tes infos, ton identifiant:  \" " . $id . "\", ton mot de passe:  \"" . $password ."\"");
+?>
