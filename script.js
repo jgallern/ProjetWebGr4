5aa86b701f404e337a4c97d6c6fdf83206678893
BQuent1 <quentin.baud9@gmail.com>
2024-03-15 16:25:20 +0100

    Page frontend

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
         setTimeout(function () {
             titre_page_connexion.innerText += '.';
         }, 1100);
-    }, 1000); 
+    }, 1000);
 })
 
 
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         // Attendre que le défilement soit terminé pour rétablir l'échelle normale
-        setTimeout(function(){
+        setTimeout(function () {
             articles.forEach(article => {
                 article.style.transform = "scale(1)"; // Rétablir l'échelle normale de chaque article
             });
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         // Attendre que le défilement soit terminé pour rétablir l'échelle normale
-        setTimeout(function(){
+        setTimeout(function () {
             articles.forEach(article => {
                 article.style.transform = "scale(1)"; // Rétablir l'échelle normale de chaque article
             });
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         // Attendre que le défilement soit terminé pour rétablir l'échelle normale
-        setTimeout(function(){
+        setTimeout(function () {
             articlesCandidatures.forEach(articleCandidatures => {
                 articleCandidatures.style.transform = "scale(1)"; // Rétablir l'échelle normale de chaque article
             });
@@ -103,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         // Attendre que le défilement soit terminé pour rétablir l'échelle normale
-        setTimeout(function(){
+        setTimeout(function () {
             articlesCandidatures.forEach(articleCandidatures => {
                 articleCandidatures.style.transform = "scale(1)"; // Rétablir l'échelle normale de chaque article
             });
@@ -113,7 +113,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const searchBar = document.getElementById('recherche_wishlist');
     const articles = document.querySelectorAll('#wishlist_container .offre_wishlist');
     var boutons_nav = document.getElementById('defilement_droite_wishlist');
@@ -122,7 +122,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-    searchBar.addEventListener('input', function() {
+    searchBar.addEventListener('input', function () {
         const searchValue = searchBar.value.trim().toLowerCase(); // Récupérer la valeur de la recherche et la convertir en minuscules
         boutons_nav.style.display = 'block';
         boutons_nav2.style.display = 'block';
@@ -140,7 +140,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
         console.log(nbArticles);
-        if(nbArticles == 0){
+        if (nbArticles == 0) {
             boutons_nav.style.display = 'none';
             boutons_nav2.style.display = 'none';
             message.style.display = 'block';
@@ -149,7 +149,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const searchBarCandidatures = document.getElementById('recherche_candidatures');
     const articles_candidatures = document.querySelectorAll('#candidatures_container .offre_candidatures');
     var boutons_nav_candidatures = document.getElementById('defilement_droite_candidatures');
@@ -158,7 +158,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-    searchBarCandidatures.addEventListener('input', function() {
+    searchBarCandidatures.addEventListener('input', function () {
         const searchValueCandidatures = searchBarCandidatures.value.trim().toLowerCase(); // Récupérer la valeur de la recherche et la convertir en minuscules
         boutons_nav_candidatures.style.display = 'block';
         boutons_nav_candidatures2.style.display = 'block';
@@ -176,11 +176,101 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
         console.log(nbarticles_candidatures);
-        if(nbarticles_candidatures == 0){
+        if (nbarticles_candidatures == 0) {
             boutons_nav_candidatures.style.display = 'none';
             boutons_nav_candidatures2.style.display = 'none';
             message_candidatures.style.display = 'block';
         }
     });
 });
+
+
+
+
+
+
+
+
+// PAGE GESTION ENTREPRISES ETUDIANTS
+
+document.addEventListener("DOMContentLoaded", function () {
+    var profil_photo = document.getElementById("photo_profil");
+    var detail_profil = document.getElementById("detail_profil");
+    var nom_prenom = document.getElementById("nom_prenom_etudiant");
+    var bouton_voir_profil = document.getElementById("bouton_voir_profil");
+    var bouton_deconnexion = document.getElementById("bouton_deconnexion");
+
+    profil_photo.addEventListener('click', function(event) {
+        if (detail_profil.offsetWidth === 0 && detail_profil.offsetHeight === 0) {
+            detail_profil.style.height = '20em';
+            detail_profil.style.width = '20em';
+            profil_photo.style.position = 'absolute';
+            profil_photo.style.right = '7.5em';
+            profil_photo.style.top = '2em';
+            profil_photo.style.width = '5em';
+            profil_photo.style.cursor = 'default';
+            nom_prenom.style.display = 'block';
+            bouton_voir_profil.style.display = 'block';
+            bouton_deconnexion.style.display = 'block';
+        }
+    });
+
+    window.addEventListener('click', function(event) {
+        if (detail_profil.offsetWidth > 0 && detail_profil.offsetHeight > 0) {
+            if (!detail_profil.contains(event.target) && event.target !== profil_photo) {
+                detail_profil.style.height = '0px';
+                detail_profil.style.width = '0px';
+                profil_photo.style.position = 'inherit';
+                profil_photo.style.right = '0em';
+                profil_photo.style.top = '0em';
+                profil_photo.style.width = '3em';
+                profil_photo.style.cursor = 'pointer';
+                nom_prenom.style.display = 'none';
+                bouton_voir_profil.style.display = 'none';
+                bouton_deconnexion.style.display = 'none';            }
+        }
+    });
+
+
+
+
+
+    var filter_button = document.getElementById("btn_filtre");
+    var formulaire_filtre = document.getElementById("form_filtre");
+    var les_filtres = document.querySelectorAll(".un_filtre");
+    var submit = document.getElementById("submit_button");
+    var deplier_ou_pas = false
+    filter_button.addEventListener('click', function () {
+        filter_button.style.borderBottomLeftRadius = '0px';
+        filter_button.style.borderBottomRightRadius = '0px';
+
+        if (deplier_ou_pas == false) {
+            les_filtres.forEach(function (filtre) {
+                filtre.style.opacity = '1';
+                filtre.style.transitionDelay = "0.4s";
+            });
+            submit.style.opacity = '1';
+            submit.style.transitionDelay = "0.4s";
+            filter_button.innerHTML = 'Fermer';
+            formulaire_filtre.style.maxHeight = formulaire_filtre.scrollHeight + 'px';
+
+            deplier_ou_pas = true;
+        }
+        else {
+            filter_button.style.borderBottomLeftRadius = '8px';
+            filter_button.style.borderBottomRightRadius = '8px';
+            les_filtres.forEach(function (filtre) {
+                filtre.style.opacity = '0';
+                filtre.style.transitionDelay = "0s";
+            });
+            submit.style.opacity = '0';
+            submit.style.transitionDelay = "0s";
+            filter_button.innerHTML = 'Filtrer';
+            formulaire_filtre.style.maxHeight = null;
+
+            deplier_ou_pas = false;
+        }
+
+    })
+})
 
