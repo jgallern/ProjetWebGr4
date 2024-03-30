93ce4a118f99b1ce430aa548c2a55a5848c4abc8
MELO <orfalim260@gmail.com>
2024-03-30 12:42:06 +0100

    Update_archi_Base
    La base de routeur...

---
+++ models/Model.php
@@ -0,0 +1,39 @@
+<?php
+
+abstract class Model
+{
+
+    private static $_bdd;
+
+    private static function setBDD()
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "bdd_projetweb";
+
+        self::$_bdd = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        self::$_bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    //Recupere la connexion a la BDD
+    protected function getBDD()
+    {
+        if (self::$_bdd == null)
+            self::setBDD();
+        return self::$_bdd;
+    }
+
+    protected function getAll($table, $obj)
+    {
+        $var = [];
+        $req = $this->getBDD()->prepare('SELECT * FROM'. $table. 'ORDER BY id desc');
+        $req->execute();
+        while ($data = $req-> fetch(PDO::FETCH_ASSOC)){
+            $var[] = new $obj($data);
+        }
+        $req->closeCursor();
+        return $var;
+
+    }
+}
