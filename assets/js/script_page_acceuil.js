7b9d129912fa254ec97058e8ba8175781f397622
BQuent1 <148761336+BQuent1@users.noreply.github.com>
2024-04-03 16:54:47 +0200

    Add files via upload

--- assets/js/script_page_acceuil.js
+++ assets/js/script_page_acceuil.js
@@ -16,8 +16,10 @@ function toggleMenu() {
     }  
 }
 
+/*   PAGE ENTREPRISES PILOTE ADMIN  */
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
+
     var profil_photo = document.getElementById("photo_profil");
     var detail_profil = document.getElementById("detail_profil");
     var nom_prenom = document.getElementById("nom_prenom_etudiant");
@@ -56,48 +58,4 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-
-
-
-
-    var filter_button = document.getElementById("btn_filtre");
-    var formulaire_filtre = document.getElementById("form_filtre");
-    var les_filtres = document.querySelectorAll(".un_filtre");
-    var submit = document.getElementById("submit_button");
-    var deplier_ou_pas = false
-    filter_button.addEventListener('click', function () {
-        filter_button.style.borderBottomLeftRadius = '0px';
-        filter_button.style.borderBottomRightRadius = '0px';
-
-        if (deplier_ou_pas == false) {
-            les_filtres.forEach(function (filtre) {
-                filtre.style.opacity = '1';
-                filtre.style.transitionDelay = "0.6s";
-            });
-            submit.style.opacity = '1';
-            submit.style.transitionDelay = "0.6s";
-            filter_button.innerHTML = 'Fermer';
-            formulaire_filtre.style.maxHeight = formulaire_filtre.scrollHeight + 'px';
-
-            deplier_ou_pas = true;
-        }
-        else {
-            setTimeout(function() {
-                filter_button.style.borderBottomLeftRadius = '8px';
-                filter_button.style.borderBottomRightRadius = '8px';
-            }, 750);
-            
-            les_filtres.forEach(function (filtre) {
-                filtre.style.opacity = '0';
-                filtre.style.transitionDelay = "0s";
-            });
-            submit.style.opacity = '0';
-            submit.style.transitionDelay = "0s";
-            filter_button.innerHTML = 'Filtrer';
-            formulaire_filtre.style.maxHeight = null;
-
-            deplier_ou_pas = false;
-        }
-
-    })
-})
+});
--- assets/js/script_page_connexion.js
+++ assets/js/script_page_connexion.js
@@ -5,3 +5,4 @@ document.addEventListener('DOMContentLoaded', function () {
     txt_bienvenue.textContent = phrase[indiceAleatoire];
 
 });
+
