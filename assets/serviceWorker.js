3a963d28e96ec054cfb19a9f1c2908effd44f5c5
gauthierwbl <gauthier.weibel@viacesi.fr>
2024-05-31 18:45:58 +0200

    ok

--- assets/serviceWorker.js
+++ assets/serviceWorker.js
@@ -1,21 +1,30 @@
-//Installation du service worker
-const CACHE_NAME = 'NomDuCache'
+const CACHE_NAME = 'NomDuCache';
+const assets = [
+    // Ajoutez ici les ressources que vous souhaitez mettre en cache
+    '../views/page_accueil_ss_connexion.php',
+    '../assets/css/style.css',
+    '../assets/js/script_wishlist_candidatures.js',
+    '../assets/images/logo_noir.png',
+    // Ajoutez d'autres fichiers nécessaires
+];
+
 self.addEventListener('install', evt => {
-    evt.waitUntil(caches.open(NomDuCache).then(cache => {
-        cache.addAll(assets);
-    })
-    )
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
 });
 
 self.addEventListener('activate', evt => {
-    console.log('le Service Worker a été installé ');
-})
+    console.log('Le Service Worker a été installé');
+});
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.open('NomDuCache').then(function (cache) {
-            return cache.match(event.request).then(function (response) {
-                return response || fetch(event.request).then(function (response) {
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request).then(response => {
                     cache.put(event.request, response.clone());
                     return response;
                 });
