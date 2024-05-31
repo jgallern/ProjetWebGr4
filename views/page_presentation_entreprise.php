381f8a92ec8f5b670380bdf01a664081aef426e1
gauthierwbl <gauthier.weibel@viacesi.fr>
2024-05-31 17:54:11 +0200

    test

--- views/page_presentation_entreprise.php
+++ views/page_presentation_entreprise.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="utf-8">
     <title>Nom Offre</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Description de votre page pour les moteurs de recherche">
+
     <link rel="stylesheet" href="../assets/css/style.css">
     <script src="script.js"></script>
     <link rel="icon" href="" type="image/png">
@@ -13,11 +16,11 @@
 
 <body>
     <nav id="navbar">
-        <div class="menu-icon" onclick="toggleMenu()">
+        <div class="menu-icon" onclick="toggleMenu()" aria-label="Menu">
             <div class="bar"></div>
             <div class="bar_2"></div>
         </div>
-        <img id="logo_seb" src="./logo_png.png" alt="logo" width="150px" />
+        <img id="logo_seb" src="./logo_png.png" alt="Logo de votre entreprise" width="150px">
         <div id="lien_navbar">
             <a class="lien_nav police_texte" href="gestion_entreprise_pilote_admin.html"
                 id="lien_entreprises_etudiants">Entreprises</a>
@@ -26,7 +29,7 @@
             <a class="lien_nav police_texte" href="gestion_entreprise_pilote_admin.html"
                 id="lien_offres_etudiants">Pilotes</a>
             <a class="lien_nav police_texte" href="gestion_entreprise_pilote_admin.html"
-                id="lien_offres_etudiants">Etudiants</a>
+                id="lien_offres_etudiants">Étudiants</a>
             <a class="lien_nav police_texte" href="page_wishlist_candidatures.html" id="lien_wishlist">Mes listes</a>
             <a class="lien_nav police_texte" href="page_wishlist_candidatures.html"
                 id="lien_candidatures">Candidatures</a>
@@ -38,26 +41,26 @@
                             echo $_COOKIE['$prenom'];
                         }
                     ?></h3>
-                <a style="text-decoration: none;" href="../controllers/deconnection_controller.php"><button id="bouton_deconnexion">Se déconnecter</button></a>
+                <a style="text-decoration: none;" href="../controllers/deconnection_controller.php"><button id="bouton_deconnexion" aria-label="Se déconnecter">Se déconnecter</button></a>
             </div>
 
             <img id="photo_profil"
                 src="C:\Users\quent\OneDrive - Association Cesi Viacesi mail\A2\04_web\Projet\images\photo_profil.png"
-                alt="photo_profil">
+                alt="Photo de profil de l'utilisateur">
         </div>
     </nav>
 
     <div id="lien_navbar_expand">
         <a class="lien_nav police_texte linking-animation delay-0" href="page_accueil_admin.html"
-            id="lien_entreprises_etudiants">Acceuil</a>
+            id="lien_entreprises_etudiants">Accueil</a>
         <a class="lien_nav police_texte linking-animation delay-1" href="gestion_entreprise_pilote_admin.html"
             id="lien_entreprises_etudiants">Entreprises</a>
         <a class="lien_nav police_texte linking-animation delay-2" href="gestion_entreprise_pilote_admin.html"
             id="lien_offres_etudiants">Offres</a>
         <a class="lien_nav police_texte linking-animation delay-3" href="gestion_entreprise_pilote_admin.html"
-            id="lien_offres_etudiants">Etudiants</a>
+            id="lien_offres_etudiants">Étudiants</a>
         <a class="lien_nav police_texte linking-animation delay-4" href="gestion_entreprise_pilote_admin.html"
-            id="lien_offres_etudiants">Etudiants</a>
+            id="lien_offres_etudiants">Étudiants</a>
         <a class="lien_nav police_texte linking-animation delay-5" href="page_wishlist_candidatures.html"
             id="lien_candidatures">Candidatures</a>
     </div>
@@ -71,7 +74,7 @@
                 style="clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%); background-color: coral;">
             </div>
             <img width="150px" id="img_offre_page_presentation"
-                href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.andrh.fr%2Fprestataires%2Fonlineformapro&psig=AOvVaw1QLVdktc6MNcY0vXlTb4j3&ust=1711034985290000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIj-jZWUg4UDFQAAAAAdAAAAABAI">
+                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.andrh.fr%2Fprestataires%2Fonlineformapro&psig=AOvVaw1QLVdktc6MNcY0vXlTb4j3&ust=1711034985290000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIj-jZWUg4UDFQAAAAAdAAAAABAI" alt="Image de l'offre">
             <h2>Nom entreprise</h2>
             <p>Ce texte contiendra les détails de l'offre en question</p>
             <h3>Lieu du stage</h3>
@@ -86,34 +89,30 @@
             <p>120</p>
             <h3>Rémunération :</h3>
             <p>4.50 euros/heure</p>
-
             <p>Annonce mise en ligne le : 09/04/2023</p>
-
-
-            <button>Candidater</button>
+            <button aria-label="Candidater">Candidater</button>
         </div>
 
         <div class="bloc_gestion police_texte">
             <form>
                 <h1>Tu y es presque !</h1><br>
-                <label>Ton CV :
-                <input type="file" accept=".pdf">
-                </label><br>
-                <label>Ta lettre de motivation :<br>
-                <input type="file">
-                </label>
-
+                <label for="cv">Ton CV :</label>
+                <input type="file" accept=".pdf" id="cv">
+                <br>
+                <label for="lettre_motivation">Ta lettre de motivation :</label><br>
+                <input type="file" id="lettre_motivation">
                 <button type="submit">ENVOYER</button>
             </form>
         </div>
-
     </main>
 
     <footer class="police_texte">
-    &copy; Stage En Bref. <br> Tous droits réservés <br>
-    <a href="mentions_legales.php">Mentions Légales</a>
-</footer>
+        &copy; Stage En Bref. <br> Tous droits réservés <br>
+        <a href="mentions_legales.php">Mentions Légales</a>
+    </footer>
 
 </body>
 
 </html>
+
+           
