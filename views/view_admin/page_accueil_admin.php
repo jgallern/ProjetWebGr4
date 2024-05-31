6b7fee72ed2e89a14940c90908fc47760d1237d1
gauthierwbl <gauthier.weibel@viacesi.fr>
2024-05-31 18:11:09 +0200

    ok

--- views/view_admin/page_accueil_admin.php
+++ views/view_admin/page_accueil_admin.php
@@ -4,15 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <title>Acceuil</title>
+    <title>Accueil</title>
     <link rel="stylesheet" href="../../assets/css/style.css">
     <script src="../../assets/js/script_wishlist_candidatures.js"></script>
     <link rel="icon" href="../../assets/images/logo_noir.png" type="image/png">
     <link rel="manifest" href="../../assets/manifest.json">
-    <a href="../view_admin/page_accueil_admin.php" class="lien_nav police_texte contraste-ameliore" id="lien_entreprises_etudiants">Acceuil</a>
-
-
 </head>
 
 <body>
@@ -32,7 +28,7 @@
             <a class="lien_nav police_texte" href="../view_admin/gestion_etudiants_admin.php"
                 id="lien_offres_etudiants">Etudiants</a>
             <a class="lien_nav police_texte" href="../view_admin/page_wishlist_candidatures_admin.php"
-                id="lien_candidatures">Wishlit et Candidatures</a>
+                id="lien_candidatures">Wishlist et Candidatures</a>
         </div>
         <div id="profil">
             <div id="detail_profil" class="police_texte">
@@ -44,14 +40,13 @@
                 <a style="text-decoration: none;" href="../controllers/deconnection_controller.php"><button
                         id="bouton_deconnexion">Se déconnecter</button></a>
             </div>
-
             <img id="photo_profil" src="../../assets/images/photo_profil.png" alt="photo_profil">
         </div>
     </nav>
 
     <div id="lien_navbar_expand">
         <a class="lien_nav police_texte linking-animation delay-0" href="../view_admin/page_accueil_admin.php"
-            id="lien_entreprises_etudiants">Acceuil</a>
+            id="lien_entreprises_etudiants">Accueil</a>
         <a class="lien_nav police_texte linking-animation delay-1" href="../view_admin/gestion_entreprise_admin.php"
             id="lien_entreprises_etudiants">Entreprises</a>
         <a class="lien_nav police_texte linking-animation delay-2" href="../view_admin/gestion_offre_admin.php"
@@ -61,23 +56,15 @@
         <a class="lien_nav police_texte linking-animation delay-4" href="../view_admin/gestion_pilotes_admin.php"
             id="lien_offres_etudiants">Pilotes</a>
         <a class="lien_nav police_texte linking-animation delay-5" href="../view_admin/page_wishlist_candidatures.php"
-            id="lien_candidatures">Wishlit et Candidatures</a>
+            id="lien_candidatures">Wishlist et Candidatures</a>
     </div>
 
     <main>
-
-        <h1 class="police_texte">Bonjour
-            <?= $_COOKIE["prenom"] ?>
-        </h1>
-
+        <h1 class="police_texte">Bonjour <?= $_COOKIE["prenom"] ?></h1>
         <h2 class="police_texte">Dernières actualités</h2>
-
         <div class="actus police_texte">
-
             <article class="offre_stage">
-                <img class="imgs_offre"
-                    src=""
-                    alt="image offre stage">
+                <img class="imgs_offre" src="" alt="image offre stage">
                 <div class="offre-details police_texte">
                     <h3 class="titre_poste">Intitulé du stage</h3>
                     <h4 class="entreprise_offre">CESI Corporation</h4>
@@ -90,9 +77,7 @@
             </article>
 
             <article class="offre_stage">
-                <img class="imgs_offre"
-                    src=""
-                    alt="image offre stage">
+                <img class="imgs_offre" src="" alt="image offre stage">
                 <div class="offre-details police_texte">
                     <h3 class="titre_poste">Intitulé du stage</h3>
                     <h4 class="entreprise_offre">CESI Corporation</h4>
@@ -103,15 +88,15 @@
                     <button class="bouton_candidater">Candidater</button>
                 </div>
             </article>
-
-        </div>
-
+            </div>
     </main>
+    
+    <footer class="police_texte">
+        &copy; Stage En Bref. <br> Tous droits réservés <br>
+        <a target="_blank" href="../mentions_legales.php">Mentions Légales</a>
+    </footer>
+
 </body>
 
-<footer class="police_texte">
-    &copy; Stage En Bref. <br> Tous droits réservés <br>
-    <a target="_blank" href="../mentions_legales.php">Mentions Légales</a>
-</footer>
-
 </html>
+
