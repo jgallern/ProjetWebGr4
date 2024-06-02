8aaf6e808c7a0e9ef74daffd7e1a55aa256f5132
gauthierwbl <gauthier.weibel@viacesi.fr>
2024-06-02 20:37:53 +0200

    ok

--- views/mentions_legales.php
+++ views/mentions_legales.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="theme-color" content="#ffeddf">
@@ -7,57 +8,59 @@
     <link rel="manifest" href="../../assets/manifest.json">
 
     <style>
-    body {
-        font-family: Arial, sans-serif;
-        line-height: 1.6;
-        margin: 0;
-        padding: 20px;
-    }
-    
-    h1, h2 {
-        color: #0086b3e0;
-    }
-    
-    p {
-        margin-bottom: 10px;
-    }
-    
-    strong {
-        font-weight: bold;
-    }
-    
-    /* Style spécifique pour les informations sur l'entreprise */
-    h2:first-of-type {
-        margin-top: 30px;
-    }
-    
-    /* Style spécifique pour les informations sur l'hébergeur */
-    h2:nth-of-type(2) {
-        margin-top: 20px;
-    }
-    
-    /* Style spécifique pour les listes à puces */
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    
-    li {
-        margin-bottom: 5px;
-    }
-    
-    /* Style spécifique pour les liens */
-    a {
-        color: #0056b3;
-        text-decoration: none;
-    }
-    
-    a:hover {
-        text-decoration: underline;
-    }
-</style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 20px;
+        }
+
+        h1,
+        h2 {
+            color: #0086b3e0;
+        }
+
+        p {
+            margin-bottom: 10px;
+        }
+
+        strong {
+            font-weight: bold;
+        }
+
+        /* Style spécifique pour les informations sur l'entreprise */
+        h2:first-of-type {
+            margin-top: 30px;
+        }
+
+        /* Style spécifique pour les informations sur l'hébergeur */
+        h2:nth-of-type(2) {
+            margin-top: 20px;
+        }
+
+        /* Style spécifique pour les listes à puces */
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        li {
+            margin-bottom: 5px;
+        }
+
+        /* Style spécifique pour les liens */
+        a {
+            color: #0056b3;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
     <title>Mentions Légales</title>
 </head>
+
 <body>
     <h1>Mentions Légales</h1>
     <h2>Informations sur l'entreprise</h2>
@@ -66,12 +69,24 @@
     <p>Forme juridique : <strong>Société à Responsabilité Limitée (SARL)</strong></p>
     <p>Capital social : <strong>6 000 000 €</strong></p>
     <p>Adresse du siège social : <strong>102 Avenue DMC, 68200 Mulhouse</strong></p>
-    
+
     <h2>Informations sur l'hébergeur</h2>
     <p>Nom de l'hébergeur : <strong>Stage en Bref</strong></p>
     <p>Adresse de l'hébergeur : <strong>102 Avenue DMC, Mulhouse, 68200</strong></p>
     <p>Numéro de téléphone de l'hébergeur : <strong>+33 7 66 08 66 79</strong></p>
     <p>Adresse e-mail de l'hébergeur : <a href="mailto:stageenbref@gmail.com"><strong>stageenbref@gmail.com</strong></a></p>
 
+    <!-- Inclusion du service worker -->
+    <script>
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('../../assets/service-worker.js')
+                .then(function (registration) {
+                    console.log('Service Worker registered with scope:', registration.scope);
+                }).catch(function (error) {
+                    console.log('Service Worker registration failed:', error);
+                });
+        }
+    </script>
 </body>
+
 </html>
