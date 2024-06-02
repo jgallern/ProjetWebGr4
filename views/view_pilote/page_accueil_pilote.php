189638cf99d8a903daa2d533b946bb12958a47b4
gauthierwbl <gauthier.weibel@viacesi.fr>
2024-06-02 20:35:28 +0200

    ok

--- views/view_pilote/page_accueil_pilote.php
+++ views/view_pilote/page_accueil_pilote.php
@@ -5,11 +5,22 @@
     <meta charset="utf-8">
     <meta name="theme-color" content="#ffeddf">
     <title>Accueil</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/style.css">
     <script src="../../assets/js/script_page_accueil_pilote.js"></script>
     <link rel="icon" href="../../assets/images/logo_noir.png" type="image/png">
     <link rel="manifest" href="../../assets/manifest.json">
-
+    <script>
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function() {
+                navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                }, function(error) {
+                    console.log('ServiceWorker registration failed: ', error);
+                });
+            });
+        }
+    </script>
 </head>
 
 <body>
@@ -19,7 +30,7 @@
             <div class="bar_2"></div>
         </div>
         <a href="../view_pilote/page_accueil_pilote.php"><img id="logo_seb" src="../../assets/images/logo_blanc.png"
-                alt="logo" width="60px   " /></a>
+                alt="logo" width="60px" /></a>
         <div id="lien_navbar">
             <a class="lien_nav police_texte" href="../view_pilote/gestion_entreprise_pilote.php"
                 id="lien_entreprises_etudiants">Entreprises</a>
@@ -30,15 +41,11 @@
         </div>
         <div id="profil">
             <div id="detail_profil" class="police_texte">
-                <h3 id="nom_prenom_etudiant"><?php
-                        if (isset($_COOKIE['$prenom'])){
-                            echo $_COOKIE['$prenom'];
-                        }
-                    ?></h3>
-                <a style="text-decoration: none;" href="../controllers/deconnection_controller.php"><button
-                        id="bouton_deconnexion">Se déconnecter</button></a>
+                <h3 id="nom_prenom_etudiant"><?php if (isset($_COOKIE['$prenom'])){ echo $_COOKIE['$prenom']; } ?></h3>
+                <a style="text-decoration: none;" href="../controllers/deconnection_controller.php">
+                    <button id="bouton_deconnexion">Se déconnecter</button>
+                </a>
             </div>
-
             <img id="photo_profil" src="../../assets/images/photo_profil.png" alt="photo_profil">
         </div>
     </nav>
@@ -55,19 +62,11 @@
     </div>
 
     <main>
-
-        <h1 class="police_texte">Bonjour
-            <?= $_COOKIE["prenom"] ?>
-        </h1>
-
+        <h1 class="police_texte">Bonjour <?= $_COOKIE["prenom"] ?></h1>
         <h2 class="police_texte">Dernières actualités</h2>
-
         <div class="actus police_texte">
-
             <article class="offre_stage">
-                <img class="imgs_offre"
-                    src=""
-                    alt="image offre stage">
+                <img class="imgs_offre" src="" alt="image offre stage">
                 <div class="offre-details police_texte">
                     <h3 class="titre_poste">Intitulé du stage</h3>
                     <h4 class="entreprise_offre">CESI Corporation</h4>
@@ -78,11 +77,8 @@
                     <button class="bouton_candidater">Candidater</button>
                 </div>
             </article>
-
             <article class="offre_stage">
-                <img class="imgs_offre"
-                    src=""
-                    alt="image offre stage">
+                <img class="imgs_offre" src="" alt="image offre stage">
                 <div class="offre-details police_texte">
                     <h3 class="titre_poste">Intitulé du stage</h3>
                     <h4 class="entreprise_offre">CESI Corporation</h4>
@@ -93,17 +89,13 @@
                     <button class="bouton_candidater">Candidater</button>
                 </div>
             </article>
-
         </div>
-
     </main>
 
-
+    <footer class="police_texte">
+        &copy; Stage En Bref. <br> Tous droits réservés <br>
+        <a target="_blank" href="../mentions_legales.php">Mentions Légales</a>
+    </footer>
 </body>
-
-<footer class="police_texte">
-    &copy; Stage En Bref. <br> Tous droits réservés <br>
-    <a target="_blank" href="../mentions_legales.php">Mentions Légales</a>
-</footer>
 
 </html>
